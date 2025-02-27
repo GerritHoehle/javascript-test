4cbb52b1c9f98a2869aad3bd966f17084a45f9be
Gerrit Höhle <gerrimehl@gmx.de>
2025-02-27 20:38:35 +0100

    .

---
+++ backend/index.js
@@ -0,0 +1,22 @@
+const loadModules = async (...paths) => {
+    const imports = paths.map((path) => import(path));
+    const modules = await Promise.all(imports);
+
+    const moduleMap = {};
+    modules.forEach((module, idx) => {
+        const modulePath = paths[idx];
+        moduleMap[modulePath] = module;
+    });
+    return moduleMap;
+};
+
+const testFigureModule = (await import('./inheritance/figures-tester.js')).testFigureModule;
+const figuresModules = await loadModules(
+    './inheritance/FiguresModule.js',
+    './inheritance/FiguresLegacyModule.js',
+);
+
+Object.entries(figuresModules).forEach(([modulePath, module]) => {
+    testFigureModule({moduleDescription: modulePath, module});
+});
+
